e19f38a01c62025071c9c8f18279a5b239031bb2
naddiemathkour <naddiemathkour@gmail.com>
2023-08-28 16:18:02 -0400

    added account_data functionality

---
+++ kraken_api/account_data/ledgers_info.py
@@ -0,0 +1,43 @@
+import requests
+import time
+import dotenv
+from api.kraken_signature import get_kraken_signature
+
+from pprint import pp
+"""
+Retrieve information about ledger entries. 50 results are returned at a time, the most recent by default.
+Formatted as:
+"result": {
+    "ledger": 
+{
+    "L4UESK-KG3EQ-UFO4T5": (ledger ID)
+{
+    "refid": "TJKLXF-PGMUI-4NTLXU",
+    "time": 1688464484.1787,
+    "type": "trade",
+    "subtype": "",
+    "aclass": "currency",
+    "asset": "ZGBP",
+    "amount": "-24.5000",
+    "fee": "0.0490",
+    "balance": "459567.9171"
+"""
+
+secrets = dotenv.dotenv_values(dotenv.find_dotenv())
+api_url = 'https://api.kraken.com'
+api_key = secrets['API_KEY_KRAKEN']
+api_sec = secrets['API_SEC_KRAKEN']
+
+def kraken_request(uri_path, data, api_key, api_sec):
+    headers = {}
+    headers['API-Key'] = api_key
+    # get_kraken_signature() as defined in the 'Authentication' section
+    headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)             
+    req = requests.post((api_url + uri_path), headers=headers, data=data)
+    return req
+
+resp = kraken_request('/0/private/Ledgers', {
+    "nonce": str(int(1000*time.time()))
+}, api_key, api_sec)
+
+pp(resp.json())
---
+++ kraken_api/account_data/trade_balance.py
@@ -0,0 +1,23 @@
+import requests
+import time
+import dotenv
+from api.kraken_signature import get_kraken_signature
+
+secrets = dotenv.dotenv_values(dotenv.find_dotenv())
+api_url = 'https://api.kraken.com'
+api_key = secrets['API_KEY_KRAKEN']
+api_sec = secrets['API_SEC_KRAKEN']
+
+def kraken_request(uri_path, data, api_key, api_sec):
+    headers = {}
+    headers['API-Key'] = api_key
+    # get_kraken_signature() as defined in the 'Authentication' section
+    headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)             
+    req = requests.post((api_url + uri_path), headers=headers, data=data)
+    return req
+
+resp = kraken_request('/0/private/TradeBalance', {
+    "nonce": str(int(1000*time.time()))
+}, api_key, api_sec)
+
+print(resp.json())
