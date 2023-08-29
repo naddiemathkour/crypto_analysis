d13540074c75c8de4ee28a7de95b23ce5dde28cb
naddiemathkour <naddiemathkour@gmail.com>
2023-08-29 15:37:09 -0400

    more functions added

--- kraken_api/App/account_data/account_data_api.py
+++ kraken_api/App/account_data/account_data_api.py
@@ -17,6 +17,12 @@ api_sec = secrets['API_SEC_KRAKEN']
 api_url = 'https://api.kraken.com'
 
 def kraken_request(uri_path, data, api_key, api_sec):
+    """
+    Authenticated requests must include both API-Key and API-Sign HTTP headers, and nonce in the request payload.
+    otp is also required in the payload if two-factor authentication (2FA) is enabled.\n
+    Authenticated requests should be signed with the "API-Sign" header, using a signature generated with your private key, nonce, encoded payload, and URI path according to:
+    Format: HMAC-SHA512 of (URI path + SHA256(nonce + POST data)) and base64 decoded secret API key
+    """
     headers = {}
     headers['API-KEY'] = api_key
     headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)
@@ -41,4 +47,22 @@ def get_extended_account_balance():
     """
     return kraken_request('/0/private/BalanceEx', {'nonce':str(int(1000*time.time()))}, api_key, api_sec).json()
 
-get_extended_account_balance()
+def get_trade_balance(asset):
+    """
+    Retrieve a summary of collateral balances, margin position valuations, equity and margin level.
+    Returned variables = {equivalent balance, trade balance, margin, unrealized net profit... see documentation for rest }
+
+    Parameters:
+    [required] nonce:int32 => 'number once' must be a changing and incrementing number with each api call.
+    [optional] asset='ASSET' => Base asset used to determine balance.
+    API Key Permissions Required: Orders and trades - Query open orders & trades and Orders and trades - Query closed orders & trades
+    """
+
+    return kraken_request('/0/private/TradeBalance', {'nonce':str(int(1000*time.time())), 'asset':asset}, api_key, api_sec).json()
+
+def get_open_orders():
+    """
+    
+    """
+
+get_trade_balance('ADA')
